b8a2ab19502e8bb5277be821ad47f40f553c443f
Julius Uusinarkaus <julius@uusinarkaus.com>
2020-01-15 21:28:48 +0200

    app now lists packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import parseJson from 'parse-json'
+import axios from 'axios'
+import Package from './components/Package'
 
-function App() {
+const App = () => {
+  const [packages, setPackages] = useState({})
+
+  console.log('moromoro')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/sample').then(response => {
+      setPackages(response.data)
+    })
+  }, [])
+
+  const rows = () =>
+    Object.values(packages).map(pckge => (
+      <Package key={pckge['Package']} pckge={pckge['Package']} />
+    ))
+
+  console.log(rows)
+  console.log('moro')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <ul>{rows()}</ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Package.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Package = ({ pckge }) => {
+  return (
+    <li>{pckge}</li>
+  )
+}
+
+export default Package
